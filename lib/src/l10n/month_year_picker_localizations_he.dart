4a64edf27e4d5f253f0313bd9de862d572cdbb9f
dorireuv <dori.public@gmail.com>
2023-04-13 14:24:02 +0300

    Add Hebrew

---
+++ lib/src/l10n/month_year_picker_localizations_he.dart
@@ -0,0 +1,15 @@
+import 'month_year_picker_localizations.dart';
+
+/// The translations for Hebrew (`he`).
+class MonthYearPickerLocalizationsHe extends MonthYearPickerLocalizations {
+  MonthYearPickerLocalizationsHe([String locale = 'he']) : super(locale);
+
+  @override
+  String get helpText => 'בחר חודש/שנה';
+
+  @override
+  String get okButtonLabel => 'אישור';
+
+  @override
+  String get cancelButtonLabel => 'ביטול';
+}
